d231e04b99093cf2ab4218d83ed963a7f7a1ab27
eternal mediocrity <nababkahtejp45@gmail.com>
2025-01-19 20:59:43 +0300

    beta

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-"use client";
+import Link from "next/link";
+import DefaultGame from "./game/defaultGame/page";
 
 export default function Home() {
-
     return (
         <>
-
+            <section>
+                <ul className="w-1/2 m-auto text-center text-third mt-80">
+                    <li className="bg-second p-3 transition-all duration-500 border border-second hover:border-third">
+                        <Link href={"./game/defaultGame"}>Default game</Link>
+                    </li>
+                </ul>
+            </section>
         </>
     );
 }
